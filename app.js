99717cd7a67a7a2e223981ab7cca42a01a29c424
praveenkompella81 <praveenkompella81@imad-jul17-ssh.c.hasura-nptel.internal>
2017-09-12 16:03:05 +0000

    half completed

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+
+function neww()
+{
+console.log('i am adding new feature');
+console.log('i am done half');
+}
 function something()
 {
 console.log('hey this function calling in js');
 }
 console.log('hello world');
 something();
+neww();
 
